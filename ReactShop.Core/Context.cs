b5d5d8e4c47bb4aa6fa328c2e73a5f4f4340722e
danridepdanri <106473655+danridepdanri@users.noreply.github.com>
2024-01-04 19:07:32 +0200

    first test asp back for discount

---
+++ ReactShop.Core/Context.cs
@@ -0,0 +1,16 @@
+﻿using ReactShop.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactShop.Core
+{
+    public class Context : DbContext
+    {
+        public DbSet<CartItem> CartItem { get; set; }
+        public DbSet<Product> Product { get; set; }
+    }
+}
---
+++ ReactShop.Core/DTOs/CartDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReactShop.Core.DTOs
+{
+    public class CartDTO
+    {
+        public CartDTO()
+        {
+        }
+
+        public decimal Subtotal { get; set; }
+        public decimal DiscountRate { get; set; }
+        public decimal DiscountValue { get; set; }
+        public decimal Total { get; set; }
+
+        public List<CartItemDTO> CartItems { get; set; }
+    }
+}
